31c15b9f40ebbe7e7ed0e889a148a3feec82e85a
Ethan Speer <speer.ethan717@gmail.com>
2022-10-17 01:17:18 -0400

    delete button, styling, cleaning code

--- renderer.js
+++ renderer.js
@@ -8,19 +8,22 @@ const func = async () => {
   
   func()
 
-const addFeedBtn = document.getElementById('addFeedBtn');
-const container = document.getElementById('container');
-const addFeedModal = document.getElementById('addFeedModal');
-const addLinkBtn = document.getElementById('addlinkbtn');
+const add_feed_button = document.getElementById('add_feed_button');
+const add_feed_modal = document.getElementById('add_feed_modal');
+const add_link_button = document.getElementById('add_link_button');
 const rssName = document.getElementById('rssname');
 const rssURL = document.getElementById('rssurl');
 var feedLinks = document.getElementById('feedlinks');
 var span = document.getElementsByClassName("close")[0];
-addFeedBtn.addEventListener('click', () => {
-    addFeedModal.style.display = "block";
+
+const container = document.getElementById('container');
+
+
+add_feed_button.addEventListener('click', () => {
+    add_feed_modal.style.display = "block";
 });
 
-addLinkBtn.addEventListener('click', () => {
+add_link_button.addEventListener('click', () => {
   addFeed();
   hideModal();
 });
@@ -30,26 +33,30 @@ span.addEventListener('click', () => {
 });
 
 window.onclick = function(event) {
-    if (event.target == addFeedModal) {
+    if (event.target == add_feed_modal) {
      hideModal();
     }
   }
 
 function hideModal() {
-  addFeedModal.style.display = "none";
+  add_feed_modal.style.display = "none";
 }
 
 function initialize() {
   if (localStorage["feedList"]) {
     feedLinks.innerHTML = localStorage["feedList"];
     console.log(feedLinks.innerHTML);
-    var edit_buttons = document.getElementsByClassName("editBtn");
+    var edit_buttons = document.getElementsByClassName("edit_button");
     for(var i = 0; i < edit_buttons.length; i++) {
         edit_buttons[i].addEventListener("click", editFeed);
     }
-    var load_buttons = document.getElementsByClassName("loadBtn");
+    var load_buttons = document.getElementsByClassName("load_button");
     for(var j = 0; j < load_buttons.length; j++) {
         load_buttons[j].addEventListener("click", loadFeed);
+    }
+    var delete_buttons = document.getElementsByClassName("delete_button");
+    for(var k = 0; k < delete_buttons.length; k++) {
+        delete_buttons[k].addEventListener("click", deleteFeed);
     }
   }
 }
@@ -58,34 +65,46 @@ function addFeed () {
   var feedName = document.getElementById("rssname").value;
   var feedText = document.createTextNode(feedName);
   var li = document.createElement("li");
-  var editBtn = document.createElement("button");
-  var loadBtn = document.createElement("button");
-  var icon = document.createElement("img");
-  var icon_link = document.createElement("img");
+  var edit_button = document.createElement("button");
+  var load_button = document.createElement("button");
+  var delete_button = document.createElement("button");
 
+  var icon_edit = document.createElement("img");
+  var icon_load = document.createElement("img");
+  var icon_delete = document.createElement("img");
 
   li.setAttribute("text-align", "center");
-  editBtn.setAttribute("class", "editBtn");
-  editBtn.addEventListener("click", editFeed);
-  loadBtn.setAttribute("class", "loadBtn");
-  loadBtn.setAttribute("type", "submit");
-  loadBtn.setAttribute("name", rssURL.value);
-  loadBtn.addEventListener("click", loadFeed);
-  icon.setAttribute("src", "./images/edit.png");
-  icon.setAttribute("class", "edit");
-  icon.setAttribute("unselectable", "on");
-  icon_link.setAttribute("src", "./images/load.png");
-  icon_link.setAttribute("class", "edit");
-  icon_link.setAttribute("unselectable", "on");
+  edit_button.setAttribute("class", "edit_button");
+  edit_button.setAttribute("title", "Edit");
+  edit_button.addEventListener("click", editFeed);
+  load_button.setAttribute("class", "load_button");
+  load_button.setAttribute("title", "Load");
+  load_button.setAttribute("name", rssURL.value);
+  load_button.addEventListener("click", loadFeed);
+  delete_button.setAttribute("class", "delete_button");
+  delete_button.setAttribute("title", "Delete");
+  delete_button.addEventListener("click", deleteFeed);
 
-  editBtn.appendChild(icon);
-  loadBtn.appendChild(icon_link);
+  icon_edit.setAttribute("src", "./images/edit.png");
+  icon_edit.setAttribute("class", "feed-button-icon");
+  icon_edit.setAttribute("unselectable", "on");
+  icon_load.setAttribute("src", "./images/load.png");
+  icon_load.setAttribute("class", "feed-button-icon");
+  icon_load.setAttribute("unselectable", "on");
+  icon_delete.setAttribute("src", "./images/delete.png");
+  icon_delete.setAttribute("class", "feed-button-icon");
+  icon_delete.setAttribute("unselectable", "on");
+
+  edit_button.appendChild(icon_edit);
+  load_button.appendChild(icon_load);
+  delete_button.appendChild(icon_delete);
   li.appendChild(feedText);
-  li.appendChild(editBtn);
-  li.appendChild(loadBtn);
+  li.appendChild(edit_button);
+  li.appendChild(load_button);
+  li.appendChild(delete_button);
   feedLinks.appendChild(li);
 
-  localStorage["feedList"] = feedLinks.innerHTML
+  localStorage["feedList"] = feedLinks.innerHTML;
   rssName.value = "";
   rssURL.value = "";
 }
@@ -116,6 +135,10 @@ function parse(data) {
     title.appendChild(xTitle)
     container.appendChild(title);
   }
+}
+
+function deleteFeed() {
+  console.log("delete");
 }
 
 window.onload = initialize;
